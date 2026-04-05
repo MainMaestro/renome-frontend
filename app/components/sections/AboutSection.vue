7d47bd23da84daa67a690667f780ae8dcc8283ba
Katy248 <petrovanton247@gmail.com>
2026-04-05 14:53:52 +0300

    Fix about section

--- app/components/sections/AboutSection.vue
+++ app/components/sections/AboutSection.vue
@@ -1,30 +1,9 @@
 <script setup lang="ts">
-import type {
-  CompanySection,
-  ListResponse,
-  TeamMember,
-  Response,
-} from "~/models";
+import type { CompanySection, Response } from "~/models";
 
 const { data: companyData } = await useApi<Response<CompanySection>>(
   "/company-section?populate=*",
 );
-const { data: teamResponse } = await useApi<ListResponse<TeamMember>>(
-  "/team-members?populate=*",
-);
-const allMembers = computed(() => teamResponse.value?.data || []);
-const displayMembers = computed(() => {
-  const members = allMembers.value || [];
-
-  return members.slice(0, 6); // Берем строго первые 6
-});
-
-const stats = [
-  { value: "5 +", label: "лет опыта" },
-  { value: "30 +", label: "сервисов в работе" },
-  { value: "50 +", label: "реализованных проектов" },
-  { value: "IT", label: "включены в реестр" },
-];
 </script>
 
 <template>
@@ -48,29 +27,31 @@ const stats = [
         <div
           class="w-full lg:w-[55%] text-[18px] leading-[140%] font-book text-black"
         >
-          <div v-if="companyData" class="whitespace-pre-line">
+          <p v-if="companyData" class="whitespace-pre-line">
             {{ companyData.data.description }}
-          </div>
+          </p>
         </div>
 
         <!-- ПРАВАЯ ЧАСТЬ: КАРТОЧКИ С ЦИФРАМИ -->
-        <div class="w-full lg:w-[45%] grid grid-cols-2 gap-6">
-          <div
-            v-for="stat in companyData?.data.characteristics"
-            :key="stat.title"
-            class="bg-gray-50 rounded-2xl flex flex-col justify-center border border-gray-100 shadow-2xl transition-all hover:shadow-md"
-          >
-            <span
-              class="text-[46px] font-normal leading-none text-renome mb-2 text-center"
+        <div class="w-full lg:w-[45%]">
+          <ul class="grid grid-cols-2 gap-6">
+            <li
+              v-for="stat in companyData?.data.characteristics"
+              :key="stat.title"
+              class="bg-gray-50 rounded-2xl flex flex-col justify-center border border-gray-100 shadow-2xl transition-all hover:shadow-md"
             >
-              {{ stat.title }}
-            </span>
-            <span
-              class="text-[14px] font-medium uppercase opacity-60 leading-tight text-black text-center"
-            >
-              {{ stat.text }}
-            </span>
-          </div>
+              <div
+                class="text-[46px] font-normal leading-none text-renome mb-2 text-center"
+              >
+                {{ stat.title }}
+              </div>
+              <div
+                class="text-[14px] font-medium uppercase opacity-60 leading-tight text-black text-center"
+              >
+                {{ stat.text }}
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
